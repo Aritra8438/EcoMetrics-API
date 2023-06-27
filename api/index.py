b3162699791cf23aa0cc31c6bc58f9f17223d3b7
Aritra8438 <aritramajumder8438@gmail.com>
2023-06-27 21:10:31 +0530

    Folder restructure

--- api/index.py
+++ api/index.py
@@ -1,7 +1,7 @@
 from flask import jsonify, request
 import json
 
-import database
+import database as database
 from models import *
 from utils.input_serializer import *
 from utils.output_serializer import serialize_queryset
@@ -12,9 +12,10 @@ app = database.app
 @app.route("/")
 def serve():
     if request.method == "GET":
-        cities, countries = region_input_manager(json.loads(request.args.get('Region')))
-        years = year_input_manager(json.loads(request.args.get('Year')))
-        queryset = Population.query.filter(Population.year.in_(years),
-                                           Population.country.in_(countries))
+        cities, countries = region_input_manager(json.loads(request.args.get("Region")))
+        years = year_input_manager(json.loads(request.args.get("Year")))
+        queryset = Population.query.filter(
+            Population.year.in_(years), Population.country.in_(countries)
+        )
         json_response = serialize_queryset(queryset)
         return jsonify(json_response)
