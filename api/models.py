0a9427b29f85fde818ac3df5fb8307ed8bc1ae2b
Aritra8438 <aritramajumder8438@gmail.com>
2023-06-27 21:03:18 +0530

    Folder restructure

--- api/models.py
+++ api/models.py
@@ -2,7 +2,7 @@ from database import db
 
 
 class Population(db.Model):
-    __tablename__ = 'population'
+    __tablename__ = "population"
     id = db.Column(db.Integer, primary_key=True)
     country = db.Column(db.String(100))
     year = db.Column(db.Integer)
