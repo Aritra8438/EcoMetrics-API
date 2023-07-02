45e312165ff4f6dbcb14482e06cd43e44914395e
Aritra8438 <aritramajumder8438@gmail.com>
2023-07-03 03:03:51 +0530

    Minor fix

--- api/models.py
+++ api/models.py
@@ -2,10 +2,6 @@ from .database import db
 
 
 class Population(db.Model):
-    # def __init__(self, _country, _year, _population):
-    #     self.country = _country
-    #     self.year = _year
-    #     self.population = _population
     __tablename__ = "population"
     id = db.Column(db.Integer, primary_key=True)
     country = db.Column(db.String(100))
