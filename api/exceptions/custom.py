31ae2aa3c37d74e06047eb5d7ca31e983010d699
Aritra8438 <aritramajumder8438@gmail.com>
2023-07-03 14:58:58 +0530

    Add Custom Exceptions

---
+++ api/exceptions/custom.py
@@ -0,0 +1,11 @@
+from werkzeug.exceptions import BadRequest
+
+
+class InvalidParameterException(Exception):
+    def __init__(self, message="There were some errors while parsing request params"):
+        super().__init__(description=message)
+
+
+class MissingParameterException(BadRequest):
+    def __init__(self, message="Some parameters might be missing from the request"):
+        super().__init__(description=message)
