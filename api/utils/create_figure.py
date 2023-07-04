40df9666cc1e9d642e64c9c32fe59f40607744ff
Aritra8438 <aritramajumder8438@gmail.com>
2023-07-04 21:55:01 +0530

    Modify themes

--- api/utils/create_figure.py
+++ api/utils/create_figure.py
@@ -1,20 +1,17 @@
 import plotly.graph_objects as go
 import plotly.express as px
 from plotly.subplots import make_subplots
-from .theme import AquaMarine, light, dark, fluroscent, blackPink
 
 
 def create_scatter(country_year_dict, country_pop_dict, user_theme):
     fig = go.Figure()
+    print(user_theme)
     theme = set_theme(user_theme)
+    print(theme)
     layout = go.Layout(
         title="Population vs Year graph",
         xaxis={"title": "Year"},
         yaxis={"title": "Population"},
-        paper_bgcolor=theme["paper_bgcolor"],
-        plot_bgcolor=theme["plot_bgcolor"],
-        font_color=theme["font_color"],
-        title_font_color=theme["title_font_color"],
     )
     for country in country_year_dict:
         fig.add_trace(
@@ -26,6 +23,14 @@ def create_scatter(country_year_dict, country_pop_dict, user_theme):
             )
         )
     fig.update_layout(layout)
+    if theme is not None:
+        layout = go.Layout(
+            paper_bgcolor=theme["paper_bgcolor"],
+            plot_bgcolor=theme["plot_bgcolor"],
+            font_color=theme["font_color"],
+            title_font_color=theme["title_font_color"],
+        )
+        fig.update_layout(layout)
     return fig.to_html(full_html=False)
 
 
@@ -40,13 +45,14 @@ def create_bar(plot_dict, user_theme):
         barmode="group",
     )
     theme = set_theme(user_theme)
-    layout = go.Layout(
-        paper_bgcolor=theme["paper_bgcolor"],
-        plot_bgcolor=theme["plot_bgcolor"],
-        font_color=theme["font_color"],
-        title_font_color=theme["title_font_color"],
-    )
-    fig.update_layout(layout)
+    if theme is not None:
+        layout = go.Layout(
+            paper_bgcolor=theme["paper_bgcolor"],
+            plot_bgcolor=theme["plot_bgcolor"],
+            font_color=theme["font_color"],
+            title_font_color=theme["title_font_color"],
+        )
+        fig.update_layout(layout)
     return fig.to_html(full_html=False)
 
 
@@ -80,25 +86,58 @@ def create_pie(array_labels1, array_labels2, num, user_theme):
         row=1,
         col=2,
     )
-    layout = go.Layout(
-        paper_bgcolor=theme["paper_bgcolor"],
-        plot_bgcolor=theme["plot_bgcolor"],
-        font_color=theme["font_color"],
-        title_font_color=theme["title_font_color"],
-    )
-    fig.update_layout(layout)
+    if theme is not None:
+        layout = go.Layout(
+            paper_bgcolor=theme["paper_bgcolor"],
+            plot_bgcolor=theme["plot_bgcolor"],
+            font_color=theme["font_color"],
+            title_font_color=theme["title_font_color"],
+        )
+        fig.update_layout(layout)
     return fig.to_html(full_html=False)
 
 
 def set_theme(user_theme):
-    if user_theme is None or user_theme == "light":
-        theme = light
-    if user_theme == "AquaMarine":
-        theme = AquaMarine
+    themes = {
+        "black_pink": {
+            "paper_bgcolor": "black",
+            "plot_bgcolor": "pink",
+            "font_color": "#E6A29E",
+            "title_font_color": "#D3382D",
+        },
+        "fluorescent": {
+            "paper_bgcolor": "#B2FF00",
+            "plot_bgcolor": "#D0FC77",
+            "font_color": "black",
+            "title_font_color": "black",
+        },
+        "aqua_marine": {
+            "paper_bgcolor": "#1E4967",
+            "plot_bgcolor": "#ADD3EC",
+            "font_color": "white",
+            "title_font_color": "white",
+        },
+        "light": {
+            "paper_bgcolor": "#A6BEBE",
+            "plot_bgcolor": "#BDD0D0",
+            "font_color": "black",
+            "title_font_color": "black",
+        },
+        "dark": {
+            "paper_bgcolor": "black",
+            "plot_bgcolor": "#383C3C",
+            "font_color": "#A5F207",
+            "title_font_color": "#A5F207",
+        },
+    }
+    if user_theme == "light":
+        return themes["light"]
+    if user_theme == "aquamarine":
+        return themes["aqua_marine"]
     if user_theme == "dark":
-        theme = dark
-    if user_theme == "fluroscent":
-        theme = fluroscent
-    if user_theme == "blackPink":
-        theme = blackPink
-    return theme
+        return themes["dark"]
+    if user_theme == "fluorescent":
+        return themes["fluorescent"]
+    if user_theme == "blackpink":
+        return themes["black_pink"]
+    return None
