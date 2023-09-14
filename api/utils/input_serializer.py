32d24af48598757f0cf27b16e852da2ad1bc931f
Aritra Majumder <aritramajumder8438@gmail.com>
2023-09-15 00:44:01 +0530

    Add docstring to `input_serializer`
    Signed-off-by: Aritra Majumder <aritramajumder8438@gmail.com>

--- api/utils/input_serializer.py
+++ api/utils/input_serializer.py
@@ -2,11 +2,23 @@ from api.exceptions.custom import InvalidParameterException
 from . import infer_region
 
 
-# Takes an array or a region, output will be two different arrays of city and country
-# Input "India" => output cities = [], countries = ["India"]
-# Input "Kolkata" => output cities = ["Kolkata"], countries = []
-# Input ["India", "Kolkata"] => output cities = ["Kolkata"], countries = ["India"]
 def region_input_manager(region_or_array):
+    """
+    Split input into cities and countries.
+
+    Given an input that can be either a string representing a city or country, or a list
+    containing a mix of city and country names, this function categorizes and separates
+    the input into two lists: one for cities and another for countries.
+
+    Example usage:
+    - Input "India" => cities = [], countries = ["India"]
+    - Input "Kolkata" => cities = ["Kolkata"], countries = []
+    - Input ["India", "Kolkata"] => cities = ["Kolkata"], countries = ["India"]
+
+    :param region_or_array: A string or list of strings representing regions, which can
+                            be cities or countries.
+    :return: A tuple containing two lists - cities and countries.
+    """
     cities = []
     countries = []
     if isinstance(region_or_array, list):
@@ -20,10 +32,21 @@ def region_input_manager(region_or_array):
     return region_input_manager([region_or_array])
 
 
-# Takes an array or a string, output will be a tuple
-# Input "population,forest_area" => Output [population,forest_area]
-# Input ["population", "forest_area"] => Output [population,forest_area]
 def compare_input_manager(comparison_params):
+    """
+    Parse input and validate requested comparisons.
+
+    This function takes either a string or a list of strings representing comparison parameters
+    and ensures that the requested comparisons are valid and properly formatted.
+
+    Example usage:
+    - Input "population,forest_area" => Output ["population", "forest_area"]
+    - Input ["population", "forest_area"] => Output ["population", "forest_area"]
+
+    :param comparison_params: A string or list of strings representing requested comparison parameters.
+    :return: A list of requested comparison parameters as strings.
+    :raises InvalidParameterException: If the input is invalid or contains unsupported comparisons.
+    """
     comparisons_available = ["gdp_per_capita", "population", "forest_area"]
     comparisons_requested = []
     if isinstance(comparison_params, list):
@@ -44,11 +67,25 @@ def compare_input_manager(comparison_params):
     return comparisons_requested
 
 
-# Takes year or tuple or array, outputs an array
-# Input 2021 => output [2021]
-# Input [2021,2022] => output [2021,2022]
-# Input "2001,2011,5" => output [2001, 2006, 2011]
 def year_input_manager(year_or_tuple_or_array, query_type="population"):
+    """
+    Parse and validate input representing years or year ranges.
+
+    This function takes input that can be either a single year, a tuple representing a
+    year range (start, stop, step), or a list of years. It validates the input and
+    returns a list of years.
+
+    Example usage:
+    - Input 2021 => Output [2021]
+    - Input [2021, 2022] => Output [2021, 2022]
+    - Input "2001,2011,5" => Output [2001, 2006, 2011]
+
+    :param year_or_tuple_or_array: Input representing years or year ranges.
+    :param query_type: The type of query for which the years are intended.
+                        Default is "population."
+    :return: A list of years.
+    :raises InvalidParameterException: If the input is invalid or contains unsupported years.
+    """
     years = []
     if isinstance(year_or_tuple_or_array, list):
         years = year_or_tuple_or_array
