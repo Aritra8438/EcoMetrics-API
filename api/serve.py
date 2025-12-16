7f17f56e38ff44f21796ce768ba4adb310aa04c9
Aritra Majumder <aritramajumder8438@gmail.com>
2025-12-17 00:24:05 +0530

    Fix linting
    Signed-off-by: Aritra Majumder <aritramajumder8438@gmail.com>

--- api/serve.py
+++ api/serve.py
@@ -2,13 +2,11 @@
 
 import sys
 import os
+from waitress import serve
 
-# Add parent directory to path to handle imports
 sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
-from waitress import serve
-from api.database import app
-import api.index  # Import to register routes
+from api.index import app  # pylint: disable=wrong-import-position
 
 if __name__ == "__main__":
     print("Starting Waitress server on port 10000...")
